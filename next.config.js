2795171cf53bd1948cdf4cf4bafa928210fd23b3
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-10-18 19:22:26 -0700

    elect form

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: "/elect",
+        destination: "https://forms.gle/D5vZ6XPFfsngCA5G8",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
